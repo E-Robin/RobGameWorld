c15a62845e752b0316cb012948e5660d3cce8e2f
E-Robin <grobin578@gmail.com>
2023-01-15 02:15:04 +0530

    files updated

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/navbar-project'),
+      dir: require('path').join(__dirname, './coverage/RobGameWorld'),
       subdir: '.',
       reporters: [
         { type: 'html' },
